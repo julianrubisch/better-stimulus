459ff2fc31dafa3f06549fb258d9f37c1557dd18
Julian Rubisch <julian@julianrubisch.at>
2020-03-24 08:31:42 +0100

    Update controller package and back to parcel

--- _src/index.js
+++ _src/index.js
@@ -1,14 +1,15 @@
 import { Application } from "stimulus";
-import * as controllers from "@hopsoft/controllers";
-import { definitionsFromContext } from "stimulus/webpack-helpers";
+import { CopyController } from "@hopsoft/controllers";
 
 import CodeAreaInstrumentationController from "./controllers/code_area_instrumentation_controller";
 
 const application = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
-application.load(definitionsFromContext(context));
 
-application.register("copy", controllers.CopyController);
+application.register(
+  "code-area-instrumentation",
+  CodeAreaInstrumentationController
+);
+application.register("copy", CopyController);
 
 document.body.dataset.controller = "code-area-instrumentation";
 document.addEventListener("turbolinks:load", e => {
