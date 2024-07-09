1281fbafcf5bc0064cdee5d526f793aa98a574d5
Julian Rubisch <julian@julianrubisch.at>
2024-07-09 14:42:59 +0200

    chore: Set default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: 'www.betterstimulus.com' }
+  Rails.application.routes.default_url_options = config.action_mailer.default_url_options
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "localhost",
