e93a16bfe8d6a908cdffa3382630500f4921cca4
Julian Rubisch <julian@julianrubisch.at>
2024-09-28 20:25:19 +0200

    chore: Move to Kamal 2.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
+  config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
@@ -92,5 +92,5 @@ Rails.application.configure do
     /.*\.betterstimulus\.com/ # Allow requests from subdomains like `www.betterstimulus.com`
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
